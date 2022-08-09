54c14fb1769dcabe19cc680bb827041e9d3229f2
SJPark97 <tjwlsdud33@naver.com>
2022-08-09 16:48:55 +0900

    add bus_info

--- 01_List1/07_4831_전기버스/s1.py
+++ 01_List1/07_4831_전기버스/s1.py
@@ -19,10 +19,11 @@ for bus_case in range (1, T + 1):
                 print(counter)                      #종점에 이동시키고 종료
                 location = N
                 break
-            elif bus_list[location + move] == 1:    #만약 버스의 최대 움직임
-                location = location + move          #위치에 충전기가 없을 경우
-                counter += 1                        #움직임 -1하고 반복
+            elif bus_list[location + move] == 1:    #만약 버스의 움직임
+                location = location + move          #위치에 충전기가 있을 경우
+                counter += 1                        #counter 1하고 반복
                 break
             elif move == 1:                         #만약 버스의 움직임
                 print(0)                            #위치에 충전기가 없을 경우
                 location = N                        #0을 출력하고 종료
+				# 버스 움직임 위치에 충전기가 없을 경우 패스 (따로 코드는 없음)
