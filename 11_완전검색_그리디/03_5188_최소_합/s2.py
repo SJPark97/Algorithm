de8dffaf3a5e2ea79963748daac5948c9f9315fd
SJPark97 <tjwlsdud33@naver.com>
2022-09-22 00:28:01 +0900

    a

--- 11_완전검색_그리디/03_5188_최소_합/s2.py
+++ 11_완전검색_그리디/03_5188_최소_합/s2.py
@@ -1,6 +1,11 @@
-def sol():
-
-
-for test_case in range(1, int(input())+1):
+for test_case in range(1, int(input()) + 1):
     print(f'#{test_case}', end=' ')
-    sol()
+    l = int(input())
+    board = [list(map(int, input().split())) for _ in range(l)]
+    for i in range(1, l):
+        board[0][i] += board[0][i - 1]
+        board[i][0] += board[i - 1][0]
+    for i in range(1, l):
+        for j in range(1, l):
+            board[i][j] += min(board[i - 1][j], board[i][j - 1])
+    print(board[l-1][l-1])
