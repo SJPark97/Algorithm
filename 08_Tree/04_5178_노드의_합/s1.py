2b11915fe3105a97453daaf363513d86b1c0f02d
SJPark97 <tjwlsdud33@naver.com>
2022-09-26 21:59:46 +0900

    a

--- 08_Tree/04_5178_노드의_합/s1.py
+++ 08_Tree/04_5178_노드의_합/s1.py
@@ -5,15 +5,15 @@ sys.stdin = open('input.txt')
 
 def sol():
     n, m, l = map(int, input().split())
-    node = [0]*(n+1)
+    node = [0] * (n + 1)
     for _ in range(m):
         a, b = map(int, input().split())
         node[a] = b
     if n % 2 == 0:
-        node[n//2] = node[n]
+        node[n // 2] = node[n]
         n -= 1
     for i in range(n, 1, -2):
-        node[i//2] = node[i-1]+node[i]
+        node[i // 2] = node[i - 1] + node[i]
     print(node[l])
     return
 
